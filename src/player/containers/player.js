9a6aca833180e7a6d81f8d2e85ff3ac7d7b6140d
andrescasas1995 <mrandres1995@hotmail.com>
2019-03-20 11:10:30 -0500

    Video PlayPause control

--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -1,16 +1,53 @@
 import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, ActivityIndicator, Text} from 'react-native';
 import Video from 'react-native-video';
 import Layout from '../components/layout';
+import ControlLayout from '../components/control-layout';
+import PlayPause from '../components/play-pause';
 
 class Player extends Component {
+    state = {
+        loading: true,
+        paused: false
+    }
+    onBuffer = (isBuffering) => {
+        this.setState({
+            loading: isBuffering
+        })
+    }
+    onLoad = () => {
+        this.setState({
+            loading: false
+        })
+    }
+    playPause = () => {
+        this.setState({
+            paused: !this.state.paused
+        })
+    }
     render(){
         return (
-            <Layout video={
+            <Layout 
+            loading={this.state.loading} 
+            video={
                 <Video 
                 source={{uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'}}
                 style={styles.video}
-                resizeMode="contain" />
+                resizeMode="contain"
+                onBuffer={this.onBuffer}
+                onLoad={this.onLoad}
+                paused={this.state.paused} />                
+            }
+            loader={
+                <ActivityIndicator color="red" />
+            }
+            controls={
+                <ControlLayout>
+                    <PlayPause onPress={this.playPause} paused={this.state.paused} />
+                    <Text>Progress bar |</Text>
+                    <Text>Time left |</Text>
+                    <Text>FullScreen |</Text>
+                </ControlLayout>
             }>
             </Layout>
         )
